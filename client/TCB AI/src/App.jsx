d3834bf8ddb3659cc9134c3657c9c974efb239f0
Aaditya Hajari <adityahazari14@gmail.com>
2025-02-19 16:39:10 +0530

    Added sidebar

---
+++ client/TCB AI/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
